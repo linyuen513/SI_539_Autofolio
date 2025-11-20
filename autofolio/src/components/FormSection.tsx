71e6f7cf94d8e3b9d910c842d42615647f1e4cd1
AnneLin <annelovedance0513@gmail.com>
2025-11-19 19:12:14 -0500

    adding css files to each components and complete Navbar design(include CSS)

---
+++ autofolio/src/components/FormSection.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import Logo from '../../src/assets/LogoWithText.svg'
+import '../App.css'
+
+function FormSection() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default FormSection
---
+++ autofolio/src/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Logo from '../../src/assets/LogoWithText.svg'
+import '../App.css'
+
+function HeroSection() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default HeroSection
